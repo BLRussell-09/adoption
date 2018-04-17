b81864d754f3a0e24815cc4eef9a56cbb450b1b9
Barry Russell <blrussell09@gmail.com>
2018-04-17 18:13:01 -0500

    Cleaned up code

--- javascripts/data.js
+++ javascripts/data.js
@@ -3,7 +3,6 @@ const dom = require('./dom');
 
 const successF = function () {
   const animals = JSON.parse(this.responseText).pets;
-  console.log('success!');
   dom.domStringBuilder(animals);
 };
 
@@ -17,35 +16,28 @@ const initializer = () => {
 
 const catSuccess = function () {
   const cats = JSON.parse(this.responseText).pets;
-  console.log('you made it to cats');
-  console.log(cats);
   dom.domCatBuilder(cats);
 };
 
 const dogSuccess = function () {
   const dogs = JSON.parse(this.responseText).pets;
-  console.log('you made it to dogs');
   dom.domDogBuilder(dogs);
 };
 
 const dinoSuccess = function () {
   const dinos = JSON.parse(this.responseText).pets;
-  console.log('you made it to dinos');
   dom.domDinoBuilder(dinos);
 };
 
 const catCall = function () {
-  console.log('you made it to cat call');
   xhr.startApp(catSuccess, errorF);
 };
 
 const doggoCall = function () {
-  console.log('you made it to cat call');
   xhr.startApp(dogSuccess, errorF);
 };
 
 const dinoCall = function () {
-  console.log('you made it to cat call');
   xhr.startApp(dinoSuccess, errorF);
 };
 
--- javascripts/events.js
+++ javascripts/events.js
@@ -6,22 +6,18 @@ const dinoButton = document.getElementById('dinos');
 const resetButton = document.getElementById('reset');
 
 const showCats = () => {
-  console.log('sup');
   data.catCall();
 };
 
 const showDogs = () => {
-  console.log('sup');
   data.doggoCall();
 };
 
 const showDinos = () => {
-  console.log('sup');
   data.dinoCall();
 };
 
 const resetPage = () => {
-  console.log('reset button');
   data.initializer();
 };
 
